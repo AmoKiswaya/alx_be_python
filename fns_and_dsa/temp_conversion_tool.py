448a8554a7b387032a7985c083f13b2005378005
AmoKiswaya <amosonganga254@gmail.com>
2025-06-04 21:55:09 -0100

    Define functions that convert temperatures between Celsius and Fahrenheit

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,23 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+user_temp = int(input("Enter the temperature to convert: ")) 
+choice = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+
+
+def convert_to_celsius(fahrenheit):
+    global  FAHRENHEIT_TO_CELSIUS_FACTOR
+    global user_temp
+    result = (user_temp - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR 
+    print(f"{user_temp} is {result}") 
+
+def convert_to_fahrenheit(celsius):
+    global CELSIUS_TO_FAHRENHEIT_FACTOR
+    global user_temp
+    result = user_temp * CELSIUS_TO_FAHRENHEIT_FACTOR + 32 
+    print(f"{user_temp} is {result}") 
+
+if choice == "F":
+    convert_to_celsius(fahrenheit=user_temp)
+else:
+    convert_to_fahrenheit(celsius=user_temp)
+
