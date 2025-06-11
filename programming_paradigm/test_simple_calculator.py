06134a89ceef51322e17f8250a38fa53cc244434
AmoKiswaya <amosonganga254@gmail.com>
2025-06-11 08:29:46 -0100

    Edit test_subtraction function

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -8,14 +8,14 @@ class TestSimpleCalculator(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(self.calc.add(-1, 1), 0) 
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 2), 3)
 
     def test_multiply(self):
         self.assertEqual(self.calc.multiply(5, 3), 15) 
 
     def test_divide(self):
-        self.assertEqual(self.calc.divide(6, 3), 2) 
+        self.assertEqual(self.calc.divide(6, 3), 2)  
         self.assertEqual(self.calc.divide(-6, 3), -2)  
         self.assertIsNone(self.calc.divide(5, 0)) 
 
