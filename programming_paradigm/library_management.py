f176a322bad8168777fda261cd48df15d187419e
AmoKiswaya <amosonganga254@gmail.com>
2025-06-11 14:34:50 -0100

    Add methods to Book class

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -4,7 +4,20 @@ class Book:
         self.author = author 
         self._is_checked_out = False  
 
+    def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+    
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
         
+    def is_available(self):
+        return not self._is_checked_out 
 
 class Library:
     def __init__(self):
@@ -16,23 +29,21 @@ class Library:
     def check_out_book(self, title):
         for book in self._books:
             if book.title == title:
-                if not book._is_checked_out:
-                    book._is_checked_out = True
+                if book.check_out():
                     return
                 else:
-                     print(f"{title} is already checked out.") 
-                     return
+                    print(f"{title} is already checked out.") 
+                    return
         print(f"Book titled {title} not found in the library.") 
 
     def return_book(self, title):
         for book in self._books:
             if book.title == title:
-                if book._is_checked_out:
-                    book._is_checked_out = False
-                    return
-                else:
-                    print(f"{title} was not checked out.") 
-                    return
+               if book.return_book():
+                   return
+               else:
+                   print(f"{title} was not checked out.")
+                   return
         print(f"Book titled {title} not found in the library.") 
 
     def list_available_books(self):
